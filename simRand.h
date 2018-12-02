24c426ccebe4848258c6ad06b92dac9c6f5edc72
katiewah <44793009+katiewah@users.noreply.github.com>
2018-12-02 01:28:38 -0500

    Create simRand.h

---
+++ simRand.h
@@ -0,0 +1,40 @@
+//
+//  Sample Discrete Event Simulation
+//
+//  Created by Richard Fujimoto on 9/24/17.
+//  Copyright © 2017 Richard Fujimoto. All rights reserved.
+//
+
+//
+// Application Independent Simulation Engine Interface
+//
+//
+// Function defined in the simulation engine called by the simulation application
+//
+
+// Call this procedure to run the simulation indicating time to end simulation
+void RunSim (double EndTime);
+
+// Schedule an event with timestamp ts, event parameters *data
+void Schedule (double ts, void *data, int custType, int nextComp);
+
+// This function returns the current simulation time
+double CurrentTime (void);
+
+void changeSchedulePQ (int type, double ts, void *data, int add);
+
+double PrioritySchedule (double ts, void *data, int custType, int nextComp, double aTime);
+
+//double returnsTime(double ts);
+
+//void bubbleSort(struct Event *start);
+
+//void swap(struct Event *a, struct Event *b);
+
+//
+// Function defined in the simulation application called by the simulation engine
+//
+//  Event handler function: called to process an event
+void EventHandler (void *data, double done);
+
+double end(double EndTime);
